f5733e5d029046ab38ca00853b7b60935ffd13de
Jericho Martinez <jerichomartinez@161.146.domi.velocityonline.net>
2017-12-02 19:52:25 -0500

    Building initial front-end

---
+++ public/js/data.js
@@ -0,0 +1,11 @@
+var chartData = [
+          {date: new Date(2011, 5, 1, 0, 0, 0, 0), val:10},
+          {date: new Date(2011, 5, 2, 0, 0, 0, 0), val:11},
+          {date: new Date(2011, 5, 3, 0, 0, 0, 0), val:12},
+          {date: new Date(2011, 5, 4, 0, 0, 0, 0), val:11},
+          {date: new Date(2011, 5, 5, 0, 0, 0, 0), val:10},
+          {date: new Date(2011, 5, 6, 0, 0, 0, 0), val:11},
+          {date: new Date(2011, 5, 7, 0, 0, 0, 0), val:13},
+          {date: new Date(2011, 5, 8, 0, 0, 0, 0), val:14},
+          {date: new Date(2011, 5, 9, 0, 0, 0, 0), val:17},
+          {date: new Date(2011, 5, 10, 0, 0, 0, 0), val:13}];
